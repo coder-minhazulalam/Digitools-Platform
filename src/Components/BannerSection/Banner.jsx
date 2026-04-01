df856fe02b934d7917f485bbecd8e88fa09325e5
coder-minhazulalam <minhazulalam409@gmail.com>
2026-04-01 21:34:29 +0600

    OverView

--- src/Components/BannerSection/Banner.jsx
+++ src/Components/BannerSection/Banner.jsx
@@ -4,20 +4,20 @@ const Banner = () => {
   return (
     <>
 
-    <div className="flex flex-col md:flex-row py-10 gap-[200px] items-center px-50">
-      <div className="flex flex-col gap-5 w-[600px] p-5 ">
-        <div className="flex gap-2 bg-[#E1E7FF] text-[#333] rounded-2xl p-2 w-[300px]">
+    <div className="flex flex-col md:flex-row py-10 gap-[10px] items-center px-30">
+      <div className="flex flex-col gap-2 w-[600px] p-5 ">
+        <div className="flex gap-2 bg-[#E1E7FF] text-[#333] rounded-2xl px-2 w-[300px]">
           <img src="/assets/Group 5.svg" alt="Group 5" />
           <p className="bg-gradient-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent">
             New: AI-Powered Tools Available
           </p>
         </div>
 
-        <h1 className="text-[60px] font-bold text-black">
+        <h1 className="text-[40px] font-bold text-black">
           Supercharge Your Digital Workflow
         </h1>
 
-        <p className="text-lg text-gray-600">
+        <p className="text-[18px] text-gray-600">
           Access premium AI tools, design assets, templates, and productivity
           software—all in one place. Start creating faster today. Explore
           Products
@@ -25,13 +25,13 @@ const Banner = () => {
 
         <div className="flex gap-3 mt-5">
           <div>
-            <button className="py-3 px-6 text-white bg-gradient-to-r from-[#4F39F6] to-[#9514FA]  rounded-3xl
+            <button className="py-2 px-4 text-white bg-gradient-to-r from-[#4F39F6] to-[#9514FA]  rounded-3xl
                   hover:scale-105 duration-300 hover:shadow-lg hover:from-[#3b2fd6] hover:to-[#7e12d4] ">
               Explore Products
             </button>
           </div>
           <div>
-            <button className="py-3 px-6 flex gap-2 border border-solid border-[#9514FA] bg-gradient-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent rounded-3xl transition-all duration-300 ease-in-out
+            <button className="py-2 px-4 flex gap-2 border border-solid border-[#9514FA] bg-gradient-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent rounded-3xl transition-all duration-300 ease-in-out
       hover:bg-[#9514FA]  hover:scale-105 ">
               <img src="/assets/Play.svg" alt="Play" /> Watch Demo
             </button>
@@ -43,7 +43,7 @@ const Banner = () => {
         <img
           src="/assets/banner.png"
           alt="hero"
-          className="w-[400px] h-[530px] mt-10 shadow-2xl rounded-3xl"
+          className="w-[400px] h-[450px] mt-10 shadow-2xl rounded-3xl"
         />
       </div>
     </div>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,8 +4,8 @@ import { FaCartShopping } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
-    <div className="flex px-[100px] sticky  top-0 justify-between items-center  py-3 bg-white shadow-md z-50">
-      <h1 className="text-[50px] font-bold bg-gradient-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent">
+    <div className="flex px-[50px] sticky  top-0 justify-between items-center  py-2 bg-white shadow-md z-50">
+      <h1 className="text-[40px] font-bold bg-gradient-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent">
         DigiTools
       </h1>
       <NavItems></NavItems>
@@ -21,7 +21,7 @@ const Navbar = () => {
         </a>
         <a
           href="/GetStarted"
-          className=" bg-gradient-to-r from-[#4F39F6] to-[#9514FA] hover:bg-blue-200 hover:text-white p-4 rounded-3xl"
+          className=" bg-gradient-to-r from-[#4F39F6] to-[#9514FA] hover:bg-blue-200 hover:text-white px-4 py-2 rounded-3xl"
         >
           Get Started
         </a>
